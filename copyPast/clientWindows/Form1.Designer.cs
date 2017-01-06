f937d13e86a42b60be1dbfb06bb9b9165500f213
Guillaume <guillaume.simonrimbault@viacesi.fr>
2017-01-06 11:33:14 +0100

    create some projects

--- copyPast/clientWindows/Form1.Designer.cs
+++ copyPast/clientWindows/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace server
+﻿namespace clientWindows
 {
     partial class Form1
     {
@@ -27,17 +27,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
             this.Text = "Form1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
